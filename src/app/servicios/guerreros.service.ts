744963363e9a8ea4251e36dfdd843dccdcf7204f
Antonio <tonymike20@hotmail.com>
2017-12-21 21:12:37 -0600

    Se actualizo la informacion de los personajes

--- src/app/servicios/guerreros.service.ts
+++ src/app/servicios/guerreros.service.ts
@@ -10,70 +10,70 @@ export class GuerrerosService {
       bio: "Vegeta, también conocido como Príncipe Vegeta o Príncipe de todos los Saiyans, es uno de los personajes principales de las sagas de Dragon Ball Z, Dragon Ball Z Kai, Dragon Ball GT y deuteragonista de Dragon Ball Super. Es el hijo mayor del Rey Vegeta, así como el príncipe superviviente del Planeta Vegeta, destruido a manos de Freezer, es el eterno rival de Son Goku, hermano mayor de Tarble, el esposo de Bulma, padre de Trunks y Bra y ancestro de Vegeta Jr.",
       img: "assets/img/Vegeta.png",
       aparicion: " Episodio 2 de Dragon Ball Z",
-      universo:"7"
+      afiliacion:"Heroe"
     },
     {
-      nombre: "Batman",
-      bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+      nombre: "Son Gohan",
+      bio: "Son Gohan es uno de los personajes principales de las sagas de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre Saiyan y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan.",
       img: "assets/img/Gohan.png",
-      aparicion: "1939-05-01",
-      universo:"7"
+      aparicion: "Episodio 1 (Dragon Ball Z)",
+      afiliacion:"Heroe"
     },
     {
-      nombre: "Daredevil",
-      bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
+      nombre: "Razuri (Androide No.18)",
+      bio: "Razuri, conocida como Androide Numero 18, es la hermana gemela del Androide Número 17 y una Androide creada a partir de una base humana por el Dr. Gero con el único fin de destruir a Goku. Esta androide se une posteriormente a los Guerreros Z y más tarde termina siendo la esposa de Krilin y la madre de Marron. A pesar de no participar en las batallas contra Majin Boo, lucha contra Super Número 17 en Dragon Ball GT con ayuda de Goku.",
       img: "assets/img/18.png",
-      aparicion: "1964-01-01",
-      universo: "7"
+      aparicion: "Anime: Episodio 132 de Dragon Ball Z",
+      afiliacion:"Heroe"
     },
     {
-      nombre: "Hulk",
-      bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
+      nombre: "Piccolo",
+      bio: "Piccolo o Piccolo Jr, conocido también como Ma Junior, es uno de los personajes principales que aparece en el manga y anime de Dragon Ball y sus correspondientes secuelas.",
       img: "assets/img/Piccolo.png",
-      aparicion: "1962-05-01",
-      universo:"7"
+      aparicion: "Episodio 123 de Dragon Ball",
+      afiliacion:"Heroe"
     },
     {
       nombre: "Son Goku",
       bio: "Son Goku, es el protagonista principal del manga y anime de Dragon Ball creado por Akira Toriyama. Su nombre de nacimiento es Kakarot y es uno de los pocos Saiyan que lograron sobrevivir a la destrucción total del Planeta Vegeta. Es el segundo hijo de Bardock y Gine, hermano menor de Raditz, nieto adoptivo de Son Gohan, esposo de Chi-Chi, padre de Son Gohan y Son Goten, a su vez también es el abuelo de Pan y ancestro de Son Goku Jr.",
       img: "assets/img/goku.png",
       aparicion: "Episodio 1 (Dragon Ball)",
-      universo: "7"
+      afiliacion:"Heroe"
     },
     {
-      nombre: "",
-      bio: "Freezer es un alienígena líder de una Organización Interplanetaria de Comercio junto a su hermano mayor, Cooler, y en secreto, su padre el Rey Cold, es el responsable de la muerte de los padres de Son Goku, y del padre de Vegeta, y de la mayor parte de su raza, los Saiyan. Por todo ello y mucho más, es considerado como el villano insignia de Dragon Ball, el archienemigo de Goku y rival de su antiguo aliado, el príncipe Vegeta.",
+      nombre: "Rapisu (Androide No.17)",
+      bio: "Rapisu conocido como Androide Número 17, es el hermano gemelo de la Androide Número 18, quien al igual que ella antes de ser Androide era un humano normal hasta que fueron secuestrados por el Dr. Gero, y es por eso que lo odian. Tras su cambio de humano a Androide, le insertaron un chip con el objetivo de destruir a Son Goku, quien en su juventud extermino al Ejército Red Ribbon. Al considerarse defectuoso porque no quería ser 'esclavo de nadie', el Dr. Gero les había colocado a ambos hermanos, un dispositivo de apagado para detenerlos en cualquier momento de desobediencia, pero cuando el científico los despierta, el rencor y rebeldía de 17 eran tal que se encargó de destruir el control que lo volvería a encerrar y acto seguido mató sin piedad a su propio creador. Su situación se le iría en contra, ya que Cell tenía como misión absorber a 17 y 18 para completar su desarrollo y convertirse en Cell Perfecto.",
       img: "assets/img/17.png",
-      aparicion: "1962-08-01",
-      universo: "7"
+      aparicion: " Episodio 133 (Dragon Ball Z)",
+      afiliacion:"Heroe"
     },
     {
-      nombre: "Wolverine",
-      bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
+      nombre: "Muten Roshi",
+      bio: "Kame-Sen'nin o Muten Roshi, conocido como Maestro Roshi, fue en su momento el Terrícola más fuerte de la Tierra, y mucha gente lo recuerda como el 'Dios de las Artes Marciales', pero antes de poder ostentar dicho título tuvo que trabajar y estudiar las artes marciales con su maestro Mutaito y su eterno rival Tsuru Sen'nin. Es el maestro y fundador del Estilo Tortuga, siendo el primer maestro tanto de Son Goku como de Krilin.",
       img: "assets/img/Maestro-roshi.png",
-      aparicion: "1974-11-01",
-      universo: "7"
+      aparicion: "Episodio 3 (Dragon Ball)",
+      afiliacion:"Heroe"
     },
     {
       nombre: "Freezer",
       bio: "Freezer es un alienígena líder de una Organización Interplanetaria de Comercio junto a su hermano mayor, Cooler, y en secreto, su padre el Rey Cold, es el responsable de la muerte de los padres de Son Goku, y del padre de Vegeta, y de la mayor parte de su raza, los Saiyan. Por todo ello y mucho más, es considerado como el villano insignia de Dragon Ball, el archienemigo de Goku y rival de su antiguo aliado, el príncipe Vegeta.",
       img: "assets/img/Freezer.png",
       aparicion: "Episodio 44 de Dragon Ball Z",
-      universo: "7"
+      afiliacion:"Villano"
     },
     {
-      nombre: "Spider-Man",
-      bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
+      nombre: "Kuririn",
+      bio: "Kuririn, es un personaje del Manga y Anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma es uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z, Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En la Saga de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron. En Dragon Ball Super se convierte en Policía de Ciudad Satán y recupera su espíritu de lucha, regresando ocasionalmente a su vida de guerrero.",
       img: "assets/img/kuririn.png",
-      aparicion: "1962-08-01",
-      universo: "7"
+      aparicion: "Episodio 14 (Dragon Ball)",
+      afiliacion:"Heroe"
     },
     {
-      nombre: "Spider-Man",
-      bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
+      nombre: "Tenshinhan",
+      bio: "Tenshinhan, conocido originalmente en Latinoamérica como Shinto[15], es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super. Tenshinhan es un luchador humano de artes marciales descendiente de un clan alienígena, que tras realizar un largo entrenamiento mental, obtuvo un tercer ojo. Es considerado uno de los individuos más poderosos provenientes de la Tierra dentro del Universo de Dragon Ball.",
       img: "assets/img/Tenshinhan.png",
-      aparicion: "1962-08-01",
-      universo: "7"
+      aparicion: "Episodio 82 (Dragon Ball)",
+      afiliacion:"Heroe"
     },
   ];
 
@@ -98,5 +98,5 @@ export interface Guerrero{
   bio: string;
   img: string;
   aparicion: string;
-  universo: string;
+  afiliacion: string;
 };
